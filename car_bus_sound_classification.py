8caf547afb02bf1c3f14ae4482114b3a9fe45168
Nhan Nguyen <nrc769@tuni.fi>
2024-12-02 19:46:15 +0200

    Updated car_bus_sound_classification.py

--- car_bus_sound_classification.py
+++ car_bus_sound_classification.py
@@ -5,3 +5,29 @@ import librosa as lb
 from scipy.fftpack import fft, ifft
 from scipy import signal
 import matplotlib.pyplot as plt
+
+
+# Reading input data
+# 1) Read sound files
+# 2) Add label to read sound files as tuple (sound, label) for each file
+
+
+
+# Preprocessing
+# 1) Normalize the dataset
+# 2) Feature extractions:
+# 2.1) Mel Spectrogram
+# 2.2) MFCC
+# 2.3) Energy: RMS
+# 2.4) Zero-crossing rate
+
+
+
+# Split the dataset into train, validation, and test data
+# 1) Train dataset
+# 2) Validation dataset
+# 3) Test dataset
+
+
+
+# Specify the model
