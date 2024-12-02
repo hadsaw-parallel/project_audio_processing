ba5675593db85bcd576a5c44ac7c770b3d938d46
hadsaw-parallel <hadsaw@in-parallel.com>
2024-12-02 23:55:50 +0200

    sucessfully loaded the files

--- car_bus_sound_classification.py
+++ car_bus_sound_classification.py
@@ -1,10 +1,11 @@
-import numpy as np
+#import numpy as np
 import soundfile as sf
 import sounddevice as sd
 import librosa as lb
 from scipy.fftpack import fft, ifft
 from scipy import signal
 import matplotlib.pyplot as plt
+import os
 
 
 # Reading and labeling input data
@@ -13,7 +14,29 @@ import matplotlib.pyplot as plt
 car = [] # [(sound, fs, label)]
 bus = [] # [(sound, fs, label)]
 
+# Define directories containing multiple audio files
+car_dir = "dataset/car-sounds"    # Replace with your car audio directory
+bus_dir = "dataset/bus-sounds"    # Replace with your bus audio directory
 
+# Load car audio files and label as 1
+for filename in os.listdir(car_dir):
+    if filename.endswith(('.wav', '.WAV')):  # Add more audio formats if needed
+        file_path = os.path.join(car_dir, filename)
+        car_audio, car_sr = sf.read(file_path)
+        car.append((car_audio, car_sr, 1))    # Label 1 for car
+
+# Load bus audio files and label as 0
+for filename in os.listdir(bus_dir):
+    if filename.endswith(('.wav', '.WAV')):  # Add more audio formats if needed
+        file_path = os.path.join(bus_dir, filename)
+        bus_audio, bus_sr = sf.read(file_path)
+        bus.append((bus_audio, bus_sr, 0))    # Label 0 for bus
+
+print(f"Loaded {len(car)} car audio files and {len(bus)} bus audio files")
+print(car[0])
+print(bus[0])
+sd.play(car[0][0], car[0][1])
+sd.wait()
 
 # Preprocessing
 # 1) Normalize the dataset
