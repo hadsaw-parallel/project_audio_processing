d4f7bacf4a1c1342807f2b72a8168b262c7cc242
Nhan Nguyen <nrc769@tuni.fi>
2024-12-02 19:37:19 +0200

    Added car_bus_sound_classification.py and some libraries in it for later use.

---
+++ car_bus_sound_classification.py
@@ -0,0 +1,7 @@
+import numpy as np
+import soundfile as sf
+import sounddevice as sd
+import librosa as lb
+from scipy.fftpack import fft, ifft
+from scipy import signal
+import matplotlib.pyplot as plt
