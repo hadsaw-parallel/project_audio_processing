679e1c2c140f7cb72e7d4838876803dde962e0f0
Nhan Nguyen <nrc769@tuni.fi>
2024-12-03 18:44:18 +0200

    Added feature_extraction.py which contains extract_feature function, and implemented preprocessing part in car_bus_sound_classification.py

--- car_bus_sound_classification.py
+++ car_bus_sound_classification.py
@@ -1,11 +1,12 @@
-#import numpy as np
+import numpy as np
 import soundfile as sf
 import sounddevice as sd
 import librosa as lb
-from scipy.fftpack import fft, ifft
+from scipy.fftpack import fft, ifft, dct
 from scipy import signal
 import matplotlib.pyplot as plt
 import os
+from feature_extraction import *
 
 
 # Reading and labeling input data
@@ -32,11 +33,13 @@ for filename in os.listdir(bus_dir):
         bus_audio, bus_sr = sf.read(file_path)
         bus.append((bus_audio, bus_sr, 0))    # Label 0 for bus
 
-print(f"Loaded {len(car)} car audio files and {len(bus)} bus audio files")
-print(car[0])
-print(bus[0])
-sd.play(car[0][0], car[0][1])
-sd.wait()
+#print(f"Loaded {len(car)} car audio files and {len(bus)} bus audio files")
+#print(car[0])
+#print(bus[0])
+#sd.play(car[0][0], car[0][1])
+#sd.wait()
+print(f'Finish reading {len(car)} car audios  and {len(bus)} bus audios.\n')
+
 
 # Preprocessing
 # 1) Normalize the dataset
@@ -45,9 +48,21 @@ sd.wait()
 # 2.2) MFCC
 # 2.3) Energy: RMS
 # 2.4) Zero-crossing rate (zcr)
-# output example: [mel spectrogram, MFCC, RMS, zcr]
-car_features = []
-bus_features = []
+# output example: list[ dict(mel spectrogram, MFCC, RMS, zcr), ...]
+Fs = 44100
+L = Fs*5
+n_fft = 2048
+win_size = 1024
+hop_size = win_size//2
+n_mels = 128
+
+car_features = extract_feature(audios=car, Fs=Fs, audio_length=L, n_fft=n_fft, win_size=win_size, hop_size=hop_size, n_mels=n_mels)
+bus_features = extract_feature(audios=bus, Fs=Fs, audio_length=L, n_fft=n_fft, win_size=win_size, hop_size=hop_size, n_mels=n_mels)
+
+print("Feature extraction done.")
+print(f'Usable car audios: {len(car_features)}. Usable bus audios: {len(bus_features)}')
+print(f'car_features[0] ->', car_features[0].keys(), "\n")
+
 
 
 
---
+++ feature_extraction.py
@@ -0,0 +1,55 @@
+import numpy as np
+import librosa as lb
+from scipy.fftpack import dct
+
+def extract_feature(audios, Fs, audio_length, n_fft, win_size, hop_size, n_mels, window_name="hamming"):
+    features = []
+    for i in range(len(audios)):
+        feature = dict()
+        audio, fs, label = audios[i]
+        
+        # Check if audio is a 1d array
+        if (audio.ndim == 1):
+            # Resample the audio to a fixed sampling rate Fs
+            if fs != Fs:
+                audio = lb.resample(audio, orig_sr=fs, target_sr=Fs)
+            
+            # Trim the audio to a fixed length of 5 seconds
+            if len(audio) != audio_length:
+                audio = audio[:audio_length]
+
+            # 1) Normalize audio to the range [0, 1]
+            min = np.min(audio)
+            max = np.max(audio)
+            audio = 2*((audio - min) / (max - min)) - 1
+            
+
+            # 2.1) Mel spectrogram in log scale
+            mel_spectro = lb.feature.melspectrogram(
+                y=audio, sr=Fs, n_fft=n_fft, n_mels=n_mels, 
+                win_length=win_size, window=window_name, hop_length=hop_size)
+            
+            mel_spectro_db = lb.power_to_db(mel_spectro, ref=np.max)
+
+            # 2.2) MFCC
+            #mfcc = lb.feature.mfcc(
+            #    y=audio, sr=Fs, n_mfcc=n_mels, n_mels=n_mels, n_fft=n_fft, 
+            #    win_length=win_size, window="hamming", hop_length=hop_size)
+            mfcc = dct(mel_spectro_db, axis=0)
+            
+            # 2.3) RMS
+            rms = lb.feature.rms(y=audio, frame_length=audio_length//hop_size, hop_length=hop_size)
+            
+            # 2.4) zcr
+            zcr = lb.feature.zero_crossing_rate(y=audio, frame_length=audio_length//hop_size, hop_length=hop_size)
+
+            # Append the features to car_features
+            feature["mel"] = mel_spectro_db
+            feature["mfcc"] = mfcc
+            feature["rms"] = rms
+            feature["zcr"] = zcr
+
+            features.append(feature)
+
+    
+    return features
